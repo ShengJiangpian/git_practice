b15ecc90b307887550306aac84589cc13b32d243
sujingpeng <sujingpeng@jsjd.cc>
2022-03-14 10:22:20 +0800

    add support.py

---
+++ support.py
@@ -0,0 +1,14 @@
+"""
+@Description : 
+@Author      : Shawn Chen
+@Copyright   : 杭州中为光电技术有限公司
+@Date        : 2022-03-11 10:38:30
+@LastEditors : Shawn Chen
+@LastEditTime: 2022-03-11 10:45:29
+@Modified    : 
+"""
+
+
+def print_func(v):
+    print("hello,", v)
+    return
